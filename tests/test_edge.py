67849f0e35b84a18db5c49ec9dafb8f1457762f3
am230 <111672334+am230@users.noreply.github.com>
2024-02-02 20:40:06 +0900

    test: edge

---
+++ tests/test_edge.py
@@ -0,0 +1,17 @@
+import pytest
+
+from edgetrans import EdgeTranslator
+
+
+@pytest.mark.asyncio
+async def test_translate():
+    translator = await EdgeTranslator.create()
+    response = await translator.translate("ja", "hello", from_lang="en")
+    assert len(response) == 1
+    text, lang = response[0]
+    assert text == "こんにちは"
+    assert lang == "ja"
+
+
+if __name__ == "__main__":
+    pytest.main()
